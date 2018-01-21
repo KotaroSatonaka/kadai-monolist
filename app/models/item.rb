a4244cff3a3c736d7077230ecd416395d1ff6420
Kotaro Satonaka <kotaro1120tomo@icloud.com>
2018-01-21 15:39:10 +0000

    want

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,5 @@ class Item < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :url, presence: true, length: { maximum: 255 }
   validates :image_url, presence: true, length: { maximum: 255 }
+  has_many :ownerships
 end
